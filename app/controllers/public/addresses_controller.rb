b2fb67afecc1d78c5d05c1be8e69d770d9b6939f
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2023-01-24 14:45:22 +0000

    [Add] public/address/view

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,12 +1,12 @@
 class Public::AddressesController < ApplicationController
   # ログインしていない場合はサインインにリダイレクト(homes/top,aboutは除く)
   before_action :authenticate_customer!
+  before_action :is_matching_login_customer, only: [:edit, :update]
 
   def index
     @customer = current_customer
     @address = Address.new
     @addresses = @customer.addresses.all
-
   end
 
   def create
@@ -21,13 +21,19 @@ class Public::AddressesController < ApplicationController
       @addresses = @customer.addresses.all
       render :index
     end
-
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -40,6 +46,15 @@ class Public::AddressesController < ApplicationController
 
   def address_params
     params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
+
+  def is_matching_login_customer
+    @address = Address.find(params[:id])
+    customer_id = @address.customer.id
+    login_customer_id = current_customer.id
+    if (customer_id != login_customer_id )
+      redirect_to addresses_path
+    end
   end
 
 end
